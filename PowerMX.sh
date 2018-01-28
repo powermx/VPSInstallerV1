6f4c6c5162fd56921dd9bdd27e8a53eb99399831
powermx <powermx@gmail.com>
2018-01-28 02:51:41 -0600

    Create PowerMX.sh

---
+++ PowerMX.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# go to root
+cd
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# install wget and curl
+apt-get update;apt-get -y install wget curl;
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service ssh restart
+
+# set repo
+wget -O /etc/apt/sources.list "https://github.com/powermx/VPSInstallerV1/blob/master/sources.list.debian6"
+wget "http://www.dotdeb.org/dotdeb.gpg"
+cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+
+# remove unused
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove sendmail*;
+apt-get -y --purge remove bind9*;
+
+# update
+apt-get update; apt-get -y upgrade;
+
+# install webserver
+apt-get -y install nginx php5-fpm php5-cli
+
+# install essential package
+apt-get -y install bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs openvpn vnstat less screen psmisc apt-file whois ptunnel ngrep mtr git zsh mrtg snmp snmpd snmp-mibs-downloader unzip unrar rsyslog debsums rkhunter
+apt-get -y install build-essential
+
+# disable exim
+service exim4 stop
+sysv-rc-conf exim4 off
+
+# update apt-file
+apt-file update
+
+# setting vnstat
+vnstat -u -i venet0
+service vnstat restart
+
+# install screenfetch
+cd
+wget https://github.com/KittyKatt/screenFetch/raw/master/screenfetch-dev
+mv screenfetch-dev /usr/bin/screenfetch
+chmod +x /usr/bin/screenfetch
+echo "clear" >> .profile
+echo "screenfetch" >> .profile
+
+# Instalacion OpenVPN
+wget -O /etc/openvpn/openvpn.tar "https://github.com/powermx/VPSInstallerV1/blob/master/openvpn-debian.tar"
+cd /etc/openvpn/
+tar xf openvpn.tar
+wget -O /etc/openvpn/1194.conf "https://github.com/powermx/VPSInstallerV1/blob/master/1194.conf"
+service openvpn restart
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+wget -O /etc/iptables.up.rules "https://github.com/powermx/VPSInstallerV1/blob/master/iptables.up.rules"
+sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
+MYIP=`curl -s ifconfig.me`;
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+sed -i $MYIP2 /etc/iptables.up.rules;
+iptables-restore < /etc/iptables.up.rules
+service openvpn restart
+
+# OpenVPN
+cd /etc/openvpn/
+wget -O /etc/openvpn/1194-client.ovpn "https://github.com/powermx/VPSInstallerV1/blob/master/1194-client.conf"
+sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
+PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
+useradd -M -s /bin/false powermx
+echo "powermx:$PASS" | chpasswd
+echo "powermx" > pass.txt
+echo "$PASS" >> pass.txt
+tar cf client.tar 1194-client.ovpn pass.txt
+cp client.tar /home/vps/public_html/
+cd
+
+# Configuracion OpenSSH
+echo "Reconfigurando OpenSSH"
+sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
+sed -i 's/Port 22/Port  22/g' /etc/ssh/sshd_config
+service ssh restart
+
+# install dropbear
+echo "Instalando Dropbear"
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109 -p 110"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+service ssh restart
+service dropbear restart
+
+# install squid
+echo "Instalando Proxy Squid"
+apt-get -y install squid
+wget -O /etc/squid/squid.conf "https://github.com/powermx/VPSInstallerV1/blob/master/squid.conf"
+sed -i $MYIP2 /etc/squid/squid.conf;
+service squid restart
+
+#Finalizando
+service ssh restart
+service dropbear restart
+service squid restart
+
+# info
+clear
+echo "VPSInstaller por PowerMX"
+echo "==============================================="
+echo "Servicios Instalados"
+echo "-------"
+echo "OpenSSH	: 22,143"
+echo "Dropbear	: 443"
+echo "Squid	: 80,8080"
+echo "OpenVPN	: TCP 1194"
+echo ""
+echo "Cuenta por Default"
+echo "Usuario: powermx"
+echo "Password: $PASS"
+echo ""
+echo "==============================================="
+echo "Para finalizar, reinicia tu VPS"
+echo "==============================================="
+echo "Android Mexico Team"
